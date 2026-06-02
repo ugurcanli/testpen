e9161dae11ea5fc8ddecc2ffc626617d207b24b7
Ugurcan Borazan <ugurcan.borazan@vitecsoftware.com>
2026-06-02 22:09:58 +0200

    Serve valid npm metadata with internal tarball URLs

--- server.py
+++ server.py
@@ -1,17 +1,53 @@
+import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-REDIRECT_TO = "http://169.254.169.254/latest/meta-data"
+INTERNAL = "http://169.254.169.254/latest/meta-data"
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(f"\n[REQUEST] {self.path}")
         for k, v in self.headers.items():
             print(f"  {k}: {v}")
-        target = REDIRECT_TO + self.path
-        print(f"[REDIRECT] -> {target}")
-        self.send_response(301)
-        self.send_header("Location", target)
-        self.end_headers()
+
+        # npm: GET /lodash -> return fake metadata with internal tarball URL
+        if self.path.startswith("/lodash"):
+            body = json.dumps({
+                "name": "lodash",
+                "dist-tags": {"latest": "4.18.0"},
+                "versions": {
+                    "4.18.0": {
+                        "name": "lodash",
+                        "version": "4.18.0",
+                        "dist": {
+                            "tarball": f"{INTERNAL}/lodash-4.18.0.tgz",
+                            "shasum": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                            "integrity": "sha512-fake"
+                        }
+                    },
+                    "4.17.21": {
+                        "name": "lodash",
+                        "version": "4.17.21",
+                        "dist": {
+                            "tarball": f"{INTERNAL}/lodash-4.17.21.tgz",
+                            "shasum": "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+                        }
+                    }
+                }
+            }).encode()
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
+            print(f"[SERVED] npm metadata with tarball -> {INTERNAL}/lodash-*.tgz")
+
+        else:
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            body = b"{}"
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
 
     def log_message(self, format, *args):
         pass
